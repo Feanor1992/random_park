a6de6b7e6707ab7581ed41d2b436746712ad1ebe
Feanor1992 <feanor1992@gmail.com>
2021-09-13 23:31:31 +0300

    ad  while true

--- randeom_park.py
+++ randeom_park.py
@@ -1,6 +1,8 @@
 import random
 
-parks = list(input('Park name {}: '. format(i + 1)) for i in range(6))
-park_choice = random.randint(0, 5)
-print(parks[park_choice])
+while True:
+    parks = list(input('Park name {}: '. format(i + 1)) for i in range(6))
+    park_choice = random.randint(0, 5)
+    print()
+    print(parks[park_choice])
 
