86126a51a205c30aa92af51d1290d7178f4cc48f
Feanor1992 <feanor1992@gmail.com>
2021-10-03 20:18:53 +0300

    add writing in file

--- randeom_park.py
+++ randeom_park.py
@@ -1,11 +1,16 @@
 import random
 
 while True:
-    parks = list(input('Park name {}: '. format(i + 1)) for i in range(6))
-    park_choice = random.randint(0, 5)
-    print()
-    print(parks[park_choice])
+    with open('Parks.txt', 'a+', encoding='utf-8') as parks_txt:
+        for i_line in parks_txt.readlines():
+            print(i_line, end='')
+
+        parks = list(input('Park name {}: '. format(i + 1)) for i in range(6))
+        park_choice = random.randint(0, 5)
+        print()
+        print(parks[park_choice])
+        parks_txt.write(parks[park_choice] + '\n')
 
     if len(parks) == 6:
-        print('программа сделал свой выбор')
+        print('программа сделала свой выбор')
         break
